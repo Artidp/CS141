972286daff1a80cb5c590b5f13487d0749ab7e57
nikhil-keshav <42218531+nikhil-keshav@users.noreply.github.com>
2018-09-07 05:58:24 +0000

    Add files via upload

---
+++ LAB5.5_Q4.cpp
@@ -0,0 +1,30 @@
+#include<iostream>		//library
+#include<string>
+
+using namespace std;
+
+
+int main(){
+
+	cout<<"Welcome to Keshav's Programming"<<endl;
+	cout<<"This progrmme makes diamond."<<endl; // intro
+
+	int a;	//declaration
+
+	cout<<"Enter the star length of diamond: ";		//asks size
+	cin>>a;
+	
+	for(int i=0;i<a;i++){		//print n rows
+		int k=0;		
+           	 
+		while(k<(a-i)){
+			//print some stars
+			cout<<"*";
+		k++;
+			
+		}
+	cout<<endl;
+	}
+	return 0;
+}
+
