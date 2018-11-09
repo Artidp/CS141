2f269de08884ddca84a150cb384bced859f569cf
nikhil-keshav <42218531+nikhil-keshav@users.noreply.github.com>
2018-11-10 00:00:23 +0530

    Add files via upload

---
+++ LAB9_Q5.cpp
@@ -0,0 +1,35 @@
+/*
+Write a piece of code which prints the characters in a cstring in a reverse order.
+*/
+#include <iostream>				//include library
+using namespace std;
+
+int main(){
+
+	int n;
+
+	cout<<"Enter size of array: "<<endl;		//asks size
+	cin>>n;
+
+	char s[n-1];			//declare array
+	cout<<"Enter characters: "<<endl;
+	cin>>s;
+
+	cout<<"Input accepted is: ";			// shows input taken
+
+	for(int i=0; i<n; i++){					// directly outputs array
+	cout<<s[i];
+	}
+
+	cout<<endl;
+
+	cout<<"Character in reverse order are: ";
+	for(char *cptr=&s[n-1]; *cptr != '\0'; cptr--){			//run loops
+		cout<<*cptr;							//print elements
+	}
+
+	cout<<endl;
+
+
+   return 0;
+}
---
+++ LAB9_Q6.cpp
@@ -0,0 +1,36 @@
+/*
+Write a function countEven(int*, int) which receives an integer array and its size, and returns the number of even numbers in the array.
+*/
+
+#include <iostream>
+using namespace std;
+
+int fun(int *arr,int n){        //take arr adress and size from main fun
+    int number=0;
+    for(int i=0;i<n;i++){          // check by loop
+		if(arr[i]%2==0){           // check for even
+			number= number +1;      // numbers of even
+		}
+	}
+	return number;
+}
+
+int main(){
+
+	int n;
+	cout<< "Enter the size of array: ";			//size of array
+	cin>>n;
+
+	int arr[n-1];			//declare array
+
+	cout << "Enter numbers: "<<endl;
+	for(int i=0;i<n;i++){		//loop to take input as per size
+		cin>> arr[i];				//takes input
+	}
+    int y = fun(arr, n);            // calls fun
+    cout<<"Number of even enteries are: ";
+	cout <<y;
+
+
+	return 0;
+}
